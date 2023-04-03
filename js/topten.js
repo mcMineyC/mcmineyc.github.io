83c2bc0170d6117bb3963f756f22b0d10244a653
mcMineyC <jedidiah.mcclimans@student.riverspringscharter.org>
2023-04-03 13:46:28 -0700

    Fill in topten

---
+++ js/topten.js
@@ -0,0 +1,48 @@
+var texts = [
+"Coding is unquestionably one of my passions.  As you can see on my Youtube channel, I've written this entire website.  I also wrote an online gallery website for my art class, so that people who couldn't make it to an in-person exhibit could still show off their art.",
+"Running simulations of fluid and smashing objects is so much fun.  I can create whatever I want, make it look however I want, and then have a video of it! Is that not cool or what?",
+"Now, you may be thinking I'm just a couch potato who happens to be intelligent, but I don't like playing mind-numbing first-person shooters.  Instead, I like games that have a puzzle in them such as Portal 2 and Superliminal.  I also like creating modern houses and cities in Minecraft."
+]
+var pintrests = ["Coding","Making 3D Animations",
+"Playing video games","Reading","Art","Building",
+"Math"]
+function a(){
+    document.getElementById("headertext").innerHTML = pintrests[0]
+    document.getElementById("textarea").innerHTML = texts[0]
+}
+function b(){
+    document.getElementById("headertext").innerHTML = pintrests[1]
+    document.getElementById("textarea").innerHTML = texts[1]
+}
+function numc(){
+    document.getElementById("headertext").innerHTML = pintrests[2]
+    document.getElementById("textarea").innerHTML = texts[2]
+}
+function d(){
+    document.getElementById("headertext").innerHTML = pintrests[3]
+    document.getElementById("textarea").innerHTML = texts[3]
+}
+function e(){
+    document.getElementById("headertext").innerHTML = pintrests[4]
+    document.getElementById("textarea").innerHTML = texts[4]
+}
+function f(){
+    document.getElementById("headertext").innerHTML = pintrests[5]
+    document.getElementById("textarea").innerHTML = texts[5]
+}
+function g(){
+    document.getElementById("headertext").innerHTML = pintrests[6]
+    document.getElementById("textarea").innerHTML = texts[6]
+}
+function h(){
+    document.getElementById("headertext").innerHTML = pintrests[7]
+    document.getElementById("textarea").innerHTML = texts[7]
+}
+function i(){
+    document.getElementById("headertext").innerHTML = pintrests[8]
+    document.getElementById("textarea").innerHTML = texts[8]
+}
+function j(){
+    document.getElementById("headertext").innerHTML = pintrests[9]
+    document.getElementById("textarea").innerHTML = texts[9]
+}
