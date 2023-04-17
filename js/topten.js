971a0faad9f701320b505e3eaf00474740e24ac1
mcMineyC <jedidiah.mcclimans@student.riverspringscharter.org>
2023-04-17 07:16:27 -0700

    u[date

--- js/topten.js
+++ js/topten.js
@@ -4,7 +4,7 @@ var texts = [
 "Yes, I do like playing video games. I don't, however, like playing mind-numbing first-person shooters.  Instead, I like games that have a puzzle in them such as Portal 2 and Superliminal.  I also like creating modern houses and cities in Minecraft.",
 "Reading transports me into another world and helps me relax.  That's really all there is to say.",
 "Art is yet another way I harness my creativity.  I really enjoy painting with acrylic.  Other ways I like to make art is through Minecraft, where I can easily make beautiful (albeit slightly blocky) dwellings.",
-"Of course, since I'm very tech-minded and have a lot of tech, I need a place to put it all.  That's where my building skills come in.  I've built myself three separate shelves (which were later repurposed into a big shelf) and a bed.  The neat thing about building something yourself is that you can customize it however you want. ",
+"Of course, since I'm very tech-minded and have a lot of tech, I need a place to put it all.  That's where my building skills come in.  This year, I built my own shelf using wood repurposed from other project.  The neat thing about building something yourself is that you can customize it however you want. ",
 "",
 "I'm interested in science because it basically explains how the world works.  As someone who wants to know how things work and why they work that way, science helps me in my pursuit of knowledge.",
 "Math.  Some kids hate it, some don't mind it.  Personally, I think it's been extremely helpful in science, as you can calculate the mass of an object, determine the half-life of a radioactive substance, and ",
@@ -12,43 +12,58 @@ var texts = [
 var pintrests = ["Coding","Making 3D Animations",
 "Playing video games","Reading","Art","Building",
 "","Science","Math"]
+var imgs = ["<img src=\"/data/topten/coding.jpg\" height=\"500px\"></img>","<img src=\"/data/topten/renders.png\" height=\"500px\"></img>",
+"<img src=\"/data/topten/coding.jpg\" height=\"500px\"></img>","<img src=\"/data/topten/coding.jpg\" height=\"500px\"></img>",
+"<img src=\"/data/topten/coding.jpg\" height=\"500px\"></img>","<img src=\"/data/topten/coding.jpg\" height=\"500px\"></img>",
+"<img src=\"/data/topten/coding.jpg\" height=\"500px\"></img>","<img src=\"/data/topten/coding.jpg\" height=\"500px\"></img>",
+"<img src=\"/data/topten/coding.jpg\" height=\"500px\"></img>","<img src=\"/data/topten/coding.jpg\" height=\"500px\"></img>",]
 function a(){
     document.getElementById("headertext").innerHTML = pintrests[0]
     document.getElementById("textarea").innerHTML = texts[0]
+    document.getElementById("img").innerHTML = imgs[0]
 }
 function b(){
     document.getElementById("headertext").innerHTML = pintrests[1]
     document.getElementById("textarea").innerHTML = texts[1]
+    document.getElementById("img").innerHTML = imgs[1]
 }
 function numc(){
     document.getElementById("headertext").innerHTML = pintrests[2]
     document.getElementById("textarea").innerHTML = texts[2]
+    document.getElementById("img").innerHTML = imgs[2]
 }
 function d(){
     document.getElementById("headertext").innerHTML = pintrests[3]
     document.getElementById("textarea").innerHTML = texts[3]
+    document.getElementById("img").innerHTML = imgs[3]
 }
 function e(){
     document.getElementById("headertext").innerHTML = pintrests[4]
     document.getElementById("textarea").innerHTML = texts[4]
+    document.getElementById("img").innerHTML = imgs[4]
 }
 function f(){
     document.getElementById("headertext").innerHTML = pintrests[5]
     document.getElementById("textarea").innerHTML = texts[5]
+    document.getElementById("img").innerHTML = imgs[5]
 }
 function g(){
     document.getElementById("headertext").innerHTML = pintrests[6]
     document.getElementById("textarea").innerHTML = texts[6]
+    document.getElementById("img").innerHTML = imgs[6]
 }
 function h(){
     document.getElementById("headertext").innerHTML = pintrests[7]
     document.getElementById("textarea").innerHTML = texts[7]
+    document.getElementById("img").innerHTML = imgs[7]
 }
 function i(){
     document.getElementById("headertext").innerHTML = pintrests[8]
     document.getElementById("textarea").innerHTML = texts[8]
+    document.getElementById("img").innerHTML = imgs[8]
 }
 function j(){
     document.getElementById("headertext").innerHTML = pintrests[9]
     document.getElementById("textarea").innerHTML = texts[9]
+    document.getElementById("img").innerHTML = imgs[9]
 }
